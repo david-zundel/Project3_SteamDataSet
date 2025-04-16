96adb11dfacb07f9a9846da10bde46a56de2da85
Silverbullet69 <jeremiah.chitwood@gmail.com>
2025-04-16 17:34:56 -0600

    Created Hash files

---
+++ src/Hash.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jeremiah on 2025-04-16.
+//
+
+#include "Hash.h"
---
+++ src/Hash.h
@@ -0,0 +1,16 @@
+//
+// Created by Jeremiah on 2025-04-16.
+//
+
+#ifndef HASH_H
+#define HASH_H
+
+
+
+class Hash {
+
+};
+
+
+
+#endif //HASH_H
